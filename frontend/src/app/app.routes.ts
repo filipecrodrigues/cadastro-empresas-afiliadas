fd8cac91017427900885e45d6f32190fcdb3e266
Filipe <filipe.candido.rodrigues@gmail.com>
2026-01-06 21:34:30 -0300

    refactor: restruturando pastas do projeto cadastro e consulta não são modais

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { CadastroComponent } from './features/empresas/components/cadastro-modal/cadastro.component';
-import { ConsultaComponent } from './features/empresas/components/consulta-modal/consulta.component';
+import { CadastroComponent } from './features/empresas/components/cadastro/cadastro.component';
+import { ConsultaComponent } from './features/empresas/components/consulta/consulta.component';
 
 
 //constante routs do tipo Routes que é um array de rodas para as páginas da aplicação
